1f2d3032aa97b8e725ed31fc376712fc5d1fcf38
stephaniemertz <stephnemertz@gmail.com>
2014-10-26 20:39:44 +0000

    newevent.php edited online with Bitbucket

--- newevent.php
+++ newevent.php
@@ -1,16 +1,37 @@
 <?php
 session_start();
 require 'database.php';
-if(isset($_POST['name'])&&isset($_POST['start'])&&isset($_POST['end'])){
-	$insert = "INSERT INTO events (name, start, end) VALUES (?, ?, ?)";
+//check session id
+
+if(isset($_POST['eventTitle'])&&isset($_POST['eventStart'])&&isset($_POST['eventEnd']&&isset($_POST['cat'])){
+	$insert = "INSERT INTO events (name, start, end, category) VALUES (?, ?, ?, ?)";
 	$stmt = $mysqli->prepare($insert);
 	if(!$stmt){
 		printf("Query Prep Failed: %s\n", $mysqli->error);
 		exit;
 	}
 	
-	$stmt->bindParam('sss', $_POST['name'], $_POST['start']), $_POST['end']); //possible error: are datetimes strings?
+	$stmt->bindParam('sss', $_POST['eventTitle'], $_POST['eventStart']), $_POST['eventEnd'], $_POST['cat']); 
 	$stmt->execute();
 	$stmt->close(); 
+	header("Location: calendar.html");
+	exit;
+}
+
+if(isset($_POST['eventTitle'])&&isset($_POST['eventStart'])&&isset($_POST['cat'])){
+	$insert = "INSERT INTO events (name, start, category) VALUES (?, ?, ?)";
+	$stmt = $mysqli->prepare($insert);
+	if(!$stmt){
+		printf("Query Prep Failed: %s\n", $mysqli->error);
+		exit;
+	}
+	
+	$stmt->bindParam('sss', $_POST['eventTitle'], $_POST['eventStart']), $_POST['cat']); //possible error: datetime format
+	$stmt->execute();
+	$stmt->close(); 
+	header("Location: calendar.html");
+}
+else{
+	header("Location: calendar.html");
 }
 ?>
