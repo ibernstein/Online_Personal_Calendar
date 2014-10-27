67029bf238ce559f48986d0b1e64c3824a202156
stephaniemertz <stephnemertz@gmail.com>
2014-10-27 03:30:01 +0000

    newevent.php edited online with Bitbucket

--- newevent.php
+++ newevent.php
@@ -2,36 +2,49 @@
 session_start();
 require 'database.php';
 //check session id
-
-if(isset($_POST['eventTitle'])&&isset($_POST['eventStart'])&&isset($_POST['eventEnd']&&isset($_POST['cat'])){
+header("Content-Type: application/json");
+if(isset($_POST['eventTitle'])&&isset($_POST['eventStart'])&&isset($_POST['eventEnd'])&&isset($_POST['cat'])){
 	$insert = "INSERT INTO events (name, start, end, category) VALUES (?, ?, ?, ?)";
 	$stmt = $mysqli->prepare($insert);
 	if(!$stmt){
 		printf("Query Prep Failed: %s\n", $mysqli->error);
+		echo json_encode(array(
+		"success" => false
+	));
 		exit;
 	}
 	
-	$stmt->bindParam('sss', $_POST['eventTitle'], $_POST['eventStart']), $_POST['eventEnd'], $_POST['cat']); 
+	$stmt->bindParam('ssss', $_POST['eventTitle'], $_POST['eventStart'], $_POST['eventEnd'], $_POST['cat']); 
 	$stmt->execute();
-	$stmt->close(); 
-	header("Location: calendar.html");
+	$stmt->close();
+	echo json_encode(array(
+		"success" => true
+	));
 	exit;
 }
 
-if(isset($_POST['eventTitle'])&&isset($_POST['eventStart'])&&isset($_POST['cat'])){
+else if(isset($_POST['eventTitle'])&&isset($_POST['eventStart'])&&isset($_POST['cat'])){
 	$insert = "INSERT INTO events (name, start, category) VALUES (?, ?, ?)";
 	$stmt = $mysqli->prepare($insert);
 	if(!$stmt){
 		printf("Query Prep Failed: %s\n", $mysqli->error);
+		echo json_encode(array(
+		"success" => false
+	));
 		exit;
 	}
 	
-	$stmt->bindParam('sss', $_POST['eventTitle'], $_POST['eventStart']), $_POST['cat']); //possible error: datetime format
+	$stmt->bindParam('sss', $_POST['eventTitle'], $_POST['eventStart'], $_POST['cat']); //possible error: datetime format
 	$stmt->execute();
-	$stmt->close(); 
-	header("Location: calendar.html");
+	$stmt->close();
+	echo json_encode(array(
+		"success" => true
+	));
+	exit();
 }
-else{
-	header("Location: calendar.html");
-}
+	echo json_encode(array(
+		"success" => false,
+		"message" => "not set"
+	));
+	exit();
 ?>
