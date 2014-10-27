5d4b700647935d0caa89c541816621ad9a168892
stephaniemertz <stephnemertz@gmail.com>
2014-10-27 04:19:47 +0000

    login.php style

--- login.php
+++ login.php
@@ -3,32 +3,39 @@
     <html lang=en>
         <head>
             <title>Welcome to Your Calendar</title>
-            <link rel="stylesheet" type="text/css" href="cal.css">
+
+             <!-- Bootstrap core CSS -->
+   			<link href="core.css" rel="stylesheet">
+
+   			 <!-- Custom styles for this template -->
+    		<link href="signinstyle.css" rel="stylesheet">
 			<script type="text/javascript">
 			//copy the create and navigate calendar here 
 			</script> 
         </head>
         <body>
-            <div>
-            <form action=login.php method="post">
-               Login with your username:<br>
-			    <label for="username">Username</label>
-                <input type="text" name="username" id= "username" maxlength="20">
-				<label for="password">Password</label>
-				<input type="password" name="password" maxlength="30">
-                <input type="submit" name="submit" value="Login">               
-            </form>
-		    <form action=signup.php method="post">
-			Signup with a new username:<br>
-			Choose a username:
-				<input type="text" name="newusername" maxlength="50"> <br>
-			Choose a password:	
-				<input type="password" name="newpassword" maxlength="30"><br>
-			Retype password:	
-				<input type="password" name="repeatpassword" maxlength="30"><br><br>
-				<button id="submit">Submit</button>
-			</form>
-			</div>
+
+        	   <div class="container">
+
+			      <form class="form-signin" action="login.php" method="post">
+			        <h2 class="form-signin-heading">Login with your username:</h2>
+			        <input type="username" name="username" class="form-control" placeholder="Username" required autofocus>
+			        <input type="password"  name="password"class="form-control" placeholder="Password" required>
+			        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+			      </form>
+
+			    </div>
+        	   <div class="container2">
+
+			      <form class="form-signup" action="signup.php" method="post">
+			        <h2 class="form-signup-heading">Signup with a new username:</h2>
+			        <input type="username" name="newusername" class="form-control" placeholder="Username" required autofocus>
+			        <input type="password"  name="newpassword" class="form-control" placeholder="Password" required>
+			        <input type="password"  name="repeatpassword" class="form-control" placeholder="Retype Password" required>
+			        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign up</button>
+			      </form>
+
+			    </div>
         </body>
     </html>
 	
@@ -54,14 +61,13 @@ if(isset($_POST['username'])&&isset($_POST['password'])){
 	if( $cnt == 1 && crypt($pwd_guess, $pwd_hash)==$pwd_hash){
 		// Login succeeded!
 		$_SESSION['user_num'] = $user_num;
-		header("Location: calendar.html");
+		header("Location: calendar");
 		exit();
 	}
 	else{
 		//login failed
 		echo "Login failed. Try again.";
-		header("Location: login.php");
+		header("Location: calendar.html");
 	}
 }
 ?>
-         
