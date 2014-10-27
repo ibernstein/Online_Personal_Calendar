7dee9a2f5843aaef98b4e3e3524b5b5543358dd9
Ian Bernstein <ibernstein578@gmail.com>
2014-10-27 00:27:21 -0500

    Calendar & New Event work, events added to database

--- newevent.php
+++ newevent.php
@@ -3,48 +3,30 @@ session_start();
 require 'database.php';
 //check session id
 header("Content-Type: application/json");
-if(isset($_POST['eventTitle'])&&isset($_POST['eventStart'])&&isset($_POST['eventEnd'])&&isset($_POST['cat'])){
-	$insert = "INSERT INTO events (name, start, end, category) VALUES (?, ?, ?, ?)";
-	$stmt = $mysqli->prepare($insert);
+if(isset($_POST['name'])&&isset($_POST['start'])&&isset($_POST['cat'])){
+	
+	$stmt = $mysqli->prepare("INSERT INTO events (name, start, end, category) VALUES (?, ?, ?, ?)");
 	if(!$stmt){
 		printf("Query Prep Failed: %s\n", $mysqli->error);
 		echo json_encode(array(
-		"success" => false
-	));
+		"success" => false,
+		"message" => "Query Prep Failed"
+		));
 		exit;
 	}
 	
-	$stmt->bindParam('ssss', $_POST['eventTitle'], $_POST['eventStart'], $_POST['eventEnd'], $_POST['cat']); 
+	$stmt->bind_param('ssss', $_POST['name'], $_POST['start'], $_POST['end'], $_POST['cat']); 
 	$stmt->execute();
 	$stmt->close();
+
 	echo json_encode(array(
 		"success" => true
 	));
 	exit;
 }
-
-else if(isset($_POST['eventTitle'])&&isset($_POST['eventStart'])&&isset($_POST['cat'])){
-	$insert = "INSERT INTO events (name, start, category) VALUES (?, ?, ?)";
-	$stmt = $mysqli->prepare($insert);
-	if(!$stmt){
-		printf("Query Prep Failed: %s\n", $mysqli->error);
-		echo json_encode(array(
-		"success" => false
-	));
-		exit;
-	}
-	
-	$stmt->bindParam('sss', $_POST['eventTitle'], $_POST['eventStart'], $_POST['cat']); //possible error: datetime format
-	$stmt->execute();
-	$stmt->close();
-	echo json_encode(array(
-		"success" => true
-	));
-	exit();
-}
 	echo json_encode(array(
 		"success" => false,
-		"message" => "not set"
+		"message" => "Not all forms filled in correctly"
 	));
-	exit();
+	exit;
 ?>
